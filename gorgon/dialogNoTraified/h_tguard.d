bcdfd8646826235bb8b5d9cc69d3a076539bfaa7
11jo <79876551+11jo@users.noreply.github.com>
2026-01-15 15:00:22 +0100

    delete dialogNoTraified

--- gorgon/dialogNoTraified/h_tguard.d
+++
@@ -1,5 +0,0 @@
-BEGIN h_tguard
-
-IF ~True()~ THEN BEGIN 0
-   SAY ~Good to see you, <CHARNAME>. You're all clear to enter.~ IF ~~ THEN EXIT
-END
--- gorgon/dialogNoTraified/h_zylard.d
+++
@@ -1,8 +0,0 @@
-BEGIN h_zylard
-
-IF ~True()~ THEN BEGIN 0
-   SAY ~As you command, Master Borland. Let this be a lesson to those who dare threaten our enterprise.~
-IF ~~ THEN DO ~
-   SetGlobal("h_SpawnZylark","GLOBAL",2)~ EXIT
-END
-
